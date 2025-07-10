83f9939ca3c05a22040f8fb5ff1af22f90262431
DGMMIND <dgmmind@gmail.com>
2025-07-09 18:44:13 -0600

    Create Index and Create, Files PHP.

---
+++ index.php
@@ -0,0 +1,10 @@
+<form action="create.php" method="POST">
+    <label>Nombre:</label>
+    <input type="text" name="user_name" required><br><br>
+
+    <label>Email:</label>
+    <input type="text" name="user_email" required><br><br>
+    <input type="text" name="user_phone" required>
+
+    <button type="submit">Registrar</button>
+</form>
