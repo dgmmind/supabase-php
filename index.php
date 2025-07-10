5c71d435eb1ce64e2e256e67f8f656dd6cb015a9
DGMMIND <dgmmind@gmail.com>
2025-07-09 19:17:51 -0600

    Update Form HTML.

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <form action="create.php" method="POST">
-    <label>Nombre:</label>
+    <label>Name:</label>
     <input type="text" name="user_name" required><br><br>
 
     <label>Email:</label>
     <input type="text" name="user_email" required><br><br>
-    <input type="text" name="user_phone" required>
-
+    <label>Phone:</label>
+    <input type="text" name="user_phone" required><br><br>
     <button type="submit">Registrar</button>
 </form>
